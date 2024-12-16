49020fd9d5ea73a9498cbcf809c9aa4c728eea70
Seb Bate <86954917+sgbstats@users.noreply.github.com>
2024-12-16 15:45:47 +0000

    updates

---
+++ run.bat
@@ -0,0 +1,13 @@
+@echo off
+
+rem Set the path to the Rscript executable
+set RSCRIPT="C:\Program Files\R\R-4.4.1\bin\Rscript.exe"
+
+rem Set the path to the R script to execute
+set RSCRIPT_FILE="C:\R\git\dreamleague\R\dl-preprocessing-run.R"
+
+rem Execute the R script
+%RSCRIPT% %RSCRIPT_FILE%
+
+rem Pause so the user can see the output
+exit
