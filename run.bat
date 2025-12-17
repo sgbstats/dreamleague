8c8d9290e2f62a15686bb80784b5047b86d2b29b
Seb <sebastiangbate@gmail.com>
2025-12-17 14:00:08 +0000

    updates and r version

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 rem Set the path to Rscript
-set RSCRIPT="C:\Program Files\R\R-4.5.0\bin\Rscript.exe"
+set RSCRIPT="C:\Program Files\R\R-4.5.2\bin\Rscript.exe"
 rem %RSCRIPT% --no-init-file -e "install.packages('vroom', type='binary', repos='https://cran.rstudio.com/')"
 
 rem Set the path to your directory
