5a61e59a5ee2db7f6a7278f30a0613203a07bd79
sgbstats <sebastiangbate@gmail.com>
2026-01-25 19:59:06 +0000

    squads

--- run.bat
+++ run.bat
@@ -1,10 +1,10 @@
 @echo off
 rem Set the path to Rscript
-set RSCRIPT="C:\Program Files\R\R-4.5.2\bin\Rscript.exe"
+set RSCRIPT="C:/Users/80010008/AppData/Local/Programs/R/R-4.5.2\bin\Rscript.exe"
 rem %RSCRIPT% --no-init-file -e "install.packages('vroom', type='binary', repos='https://cran.rstudio.com/')"
 
 rem Set the path to your directory
-cd C:/R/git/dreamleague
+cd C:\Users\80010008\git\dreamleague
 
 
 set RSCRIPT_FILE="R\dl-preprocessing-run.R"
